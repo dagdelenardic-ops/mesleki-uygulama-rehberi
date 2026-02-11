2b758bb79a41b82d08b2e04eeaf5f3de015b978e
gurursonmez90 <gurursonmez@gmail.com>
2026-02-11 09:32:48 +0300

    Komisyon ortak veri URL ayarını Firebase endpoint ile güncelle

--- commission-sync-config.js
+++ commission-sync-config.js
@@ -1,1 +1,2 @@
-window.STAJ_COMMISSION_REMOTE_URL = "";
+window.STAJ_COMMISSION_REMOTE_URL =
+  "https://mesleki-uygulama-b8b91-default-rtdb.europe-west1.firebasedatabase.app/mesleki-uygulama.json";
